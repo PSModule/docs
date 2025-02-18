1e4160a2d2e3b829cbdef4a3cd6e7f1c881b2c81
Marius Storhaug <marstor@hotmail.com>
2025-02-18 17:48:13 +0100

    Add performance testing scripts for various PowerShell operations

---
+++ guidance/PSCallStack.ps1
@@ -0,0 +1,33 @@
+﻿function Invoke-Function4 {
+    [CmdletBinding()]
+    param()
+    'In: ' + $MyInvocation.InvocationName
+    Get-PSCallStack
+}
+
+function Invoke-Function3 {
+    [CmdletBinding()]
+    param()
+    'In: ' + $MyInvocation.InvocationName
+    Get-PSCallStack
+    Invoke-Function4
+}
+
+function Invoke-Function2 {
+    [CmdletBinding()]
+    param()
+    'In: ' + $MyInvocation.InvocationName
+    Get-PSCallStack
+    Invoke-Function3
+}
+
+function Invoke-Function1 {
+    [CmdletBinding()]
+    param()
+    "In: " + $MyInvocation.InvocationName
+    Get-PSCallStack
+    Invoke-Function2
+}
+
+# Test the functions
+Invoke-Function1
---
+++ guidance/PSCmdlet.ps1
@@ -0,0 +1,33 @@
+﻿function Invoke-Function4 {
+    [CmdletBinding()]
+    param()
+    '4: ' + $MyInvocation.InvocationName
+    $PSCmdlet | ConvertTo-Json
+}
+
+function Invoke-Function3 {
+    [CmdletBinding()]
+    param()
+    '3: ' + $MyInvocation.InvocationName
+    $PSCmdlet | ConvertTo-Json
+    Invoke-Function4
+}
+
+function Invoke-Function2 {
+    [CmdletBinding()]
+    param()
+    '2: ' + $MyInvocation.InvocationName
+    $PSCmdlet | ConvertTo-Json
+    Invoke-Function3
+}
+
+function Invoke-Function1 {
+    [CmdletBinding()]
+    param()
+    '1: ' + $MyInvocation.InvocationName
+    $PSCmdlet | ConvertTo-Json
+    Invoke-Function2
+}
+
+# Test the functions
+Invoke-Function1
