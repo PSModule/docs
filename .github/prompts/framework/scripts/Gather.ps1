2953c4f928509cf1ee931d906d34579ffb4b8914
Marius Storhaug <marstor@hotmail.com>
2025-10-01 08:13:33 +0200

    chore: remove outdated instruction files and scripts for framework updates

--- .github/prompts/framework/scripts/Gather.ps1
+++
@@ -1,6 +0,0 @@
-﻿[PSCustomObject]@{
-    RepositoryPath             = [System.IO.Path]::GetFullPath("$PSScriptRoot/../../../..")
-    InstructionsPath           = [System.IO.Path]::GetFullPath("$PSScriptRoot/../../../instructions")
-    FrameworkInstructionsPath  = [System.IO.Path]::GetFullPath("$PSScriptRoot/../../../instructions/framework")
-    RepositoryInstructionsPath = [System.IO.Path]::GetFullPath("$PSScriptRoot/../../../instructions/repo")
-} | Format-List
